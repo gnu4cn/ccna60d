1a6966aa583e525b2f6d1971738063a250101060
Peng Hailin <unisko@gmail.com>
2017-02-20 17:33:47 +0800

    Freeplane mindmaps added.

---
+++ mindmaps/STP,Spanning Tree Protocol,生成树协议.mm
@@ -0,0 +1,82 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="STP,Spanning Tree Protocol,&#x751f;&#x6210;&#x6811;&#x534f;&#x8bae;" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1487582854229" TEXT_SHORTENED="true"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3"/>
+<node TEXT="&#x89d2;&#x8272;" POSITION="right" ID="ID_1259001063" CREATED="1487582630594" MODIFIED="1487583170790" HGAP="0" VSHIFT="-130">
+<edge COLOR="#ff0000"/>
+<richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      
+    </p>
+    <p>
+      &#22312;&#20855;&#22791;&#20887;&#20313;&#30340;&#20132;&#25442;&#32593;&#32476;&#25299;&#25169;&#20013;&#65292;&#20801;&#35768;&#23384;&#22312;&#22810;&#26465;&#29289;&#29702;&#38142;&#36335;&#30340;&#21516;&#26102;, &#36890;&#36807;&#24314;&#31435;&#19968;&#20010;&#26080;&#24490;&#29615;&#36923;&#36753;&#25299;&#25169;&#65292;&#38459;&#27490;&#32593;&#32476;&#19978;&#24490;&#29615;&#30340;&#21457;&#29983;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="IEEE 802.1D" POSITION="left" ID="ID_756013575" CREATED="1487582758796" MODIFIED="1487582776481">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="BPDU,Bridge Protocol DU, &#x6865;&#x534f;&#x8bae;&#x6570;&#x636e;&#x5355;&#x5143;" POSITION="right" ID="ID_1398715153" CREATED="1487582872175" MODIFIED="1487582930474">
+<edge COLOR="#00ff00"/>
+<node TEXT="&#x95f4;&#x9694;&#xff1a;2s" ID="ID_1833352132" CREATED="1487582937985" MODIFIED="1487582951229"/>
+<node TEXT="&#x5185;&#x5bb9;&#xff1a;&#x7aef;&#x53e3;&#x3001;&#x5730;&#x5740;&#x3001;&#x4f18;&#x5148;&#x7ea7;&#x53ca;&#x5f00;&#x9500;&#x7b49;&#x4fe1;&#x606f;" ID="ID_730597063" CREATED="1487582962344" MODIFIED="1487582996090"/>
+</node>
+</node>
+</map>
