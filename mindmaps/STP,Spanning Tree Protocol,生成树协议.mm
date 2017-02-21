e3fc08a5e3f13f313edc0463ea34a1b19090fa46
Peng Hailin <unisko@gmail.com>
2017-02-21 09:24:33 +0800

    Daily update. 2017-02-21 09:24:33

--- mindmaps/STP,Spanning Tree Protocol,生成树协议.mm
+++ mindmaps/STP,Spanning Tree Protocol,生成树协议.mm
@@ -50,7 +50,7 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="3"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="6"/>
 <richcontent TYPE="NOTE">
 
 <html>
@@ -65,8 +65,8 @@
 </html>
 
 </richcontent>
-<node TEXT="&#x89d2;&#x8272;" POSITION="right" ID="ID_1259001063" CREATED="1487582630594" MODIFIED="1487636290029" HGAP="0" VSHIFT="-130">
-<edge COLOR="#ff0000"/>
+<node TEXT="BPDU" POSITION="right" ID="ID_1398715153" CREATED="1487582872175" MODIFIED="1487639681771">
+<edge COLOR="#00ff00"/>
 <richcontent TYPE="NOTE">
 
 <html>
@@ -75,12 +75,48 @@
   </head>
   <body>
     <p>
-      &#22312;&#20855;&#22791;&#20887;&#20313;&#30340;&#20132;&#25442;&#32593;&#32476;&#25299;&#25169;&#20013;&#65292;&#20801;&#35768;&#23384;&#22312;&#22810;&#26465;&#29289;&#29702;&#38142;&#36335;&#30340;&#21516;&#26102;, &#36890;&#36807;&#24314;&#31435;&#19968;&#20010;&#26080;&#24490;&#29615;&#36923;&#36753;&#25299;&#25169;&#65292;&#38459;&#27490;&#32593;&#32476;&#19978;&#24490;&#29615;&#30340;&#21457;&#29983;
+      Bridge Protocol DU, &#26725;&#21327;&#35758;&#25968;&#25454;&#21333;&#20803;
     </p>
   </body>
 </html>
 
 </richcontent>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_161614335" MIDDLE_LABEL="&#x5229;&#x7528;BPDUs&#x7684;&#x4ea4;&#x6362;&#xff0c;&#x6765;&#x786e;&#x5b9a;&#x7f51;&#x7edc;&#x62d3;&#x6251;" STARTINCLINATION="61;0;" ENDINCLINATION="61;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<node TEXT="&#x95f4;&#x9694;&#xff1a;2s" ID="ID_1833352132" CREATED="1487582937985" MODIFIED="1487582951229"/>
+<node TEXT="&#x5185;&#x5bb9;&#xff1a;&#x7aef;&#x53e3;&#x3001;&#x5730;&#x5740;&#x3001;&#x4f18;&#x5148;&#x7ea7;&#x53ca;&#x5f00;&#x9500;&#x7b49;&#x4fe1;&#x606f;" ID="ID_730597063" CREATED="1487582962344" MODIFIED="1487582996090"/>
+<node TEXT="&#x5176;&#x5b83;&#xff1a;BPDUs&#x4f1a;&#x88ab;&#x6253;&#x4e0a;VLAN ID&#x6807;&#x8bb0;" ID="ID_1909260872" CREATED="1487583330497" MODIFIED="1487583355535"/>
+<node TEXT="&#x7c7b;&#x578b;" ID="ID_938846571" CREATED="1487639792052" MODIFIED="1487639925835">
+<node TEXT="&#x914d;&#x7f6e;BPDUs" ID="ID_1186452463" CREATED="1487639820753" MODIFIED="1487639925835"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      STP Configuration BPDUs
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="&#x62d3;&#x6251;&#x53d8;&#x5316;&#x901a;&#x77e5;BPDUs" ID="ID_785070455" CREATED="1487639851933" MODIFIED="1487639917210"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      STP Topology Change Notification BPDUs
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
 </node>
 <node TEXT="IEEE&#x89c4;&#x683c;" POSITION="left" ID="ID_756013575" CREATED="1487582758796" MODIFIED="1487636320588">
 <edge COLOR="#0000ff"/>
@@ -99,8 +135,8 @@
 
 </richcontent>
 </node>
-<node TEXT="BPDU" POSITION="right" ID="ID_1398715153" CREATED="1487582872175" MODIFIED="1487636262734">
-<edge COLOR="#00ff00"/>
+<node TEXT="&#x539f;&#x7406;" POSITION="left" ID="ID_1061403770" CREATED="1487636579645" MODIFIED="1487636631259">
+<edge COLOR="#ff00ff"/>
 <richcontent TYPE="NOTE">
 
 <html>
@@ -109,15 +145,160 @@
   </head>
   <body>
     <p>
-      Bridge Protocol DU, &#26725;&#21327;&#35758;&#25968;&#25454;&#21333;&#20803;
+      <span http-equiv="content-type" content="text/html; charset=utf-8" style="color: rgb(51, 51, 51); font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol; font-size: 16px; font-style: normal; font-weight: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none"><font color="rgb(51, 51, 51)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol" size="16px">&#21033;&#29992;BPDUs&#30340;&#20132;&#25442;&#65292;&#26469;&#30830;&#23450;&#32593;&#32476;&#25299;&#25169;&#12290;</font></span>
     </p>
   </body>
 </html>
 
 </richcontent>
-<node TEXT="&#x95f4;&#x9694;&#xff1a;2s" ID="ID_1833352132" CREATED="1487582937985" MODIFIED="1487582951229"/>
-<node TEXT="&#x5185;&#x5bb9;&#xff1a;&#x7aef;&#x53e3;&#x3001;&#x5730;&#x5740;&#x3001;&#x4f18;&#x5148;&#x7ea7;&#x53ca;&#x5f00;&#x9500;&#x7b49;&#x4fe1;&#x606f;" ID="ID_730597063" CREATED="1487582962344" MODIFIED="1487582996090"/>
-<node TEXT="&#x5176;&#x5b83;&#xff1a;BPDUs&#x4f1a;&#x88ab;&#x6253;&#x4e0a;VLAN ID&#x6807;&#x8bb0;" ID="ID_1909260872" CREATED="1487583330497" MODIFIED="1487583355535"/>
+</node>
+<node TEXT="&#x7f51;&#x7edc;&#x62d3;&#x6251;" POSITION="right" ID="ID_161614335" CREATED="1487636636477" MODIFIED="1487639720082">
+<edge COLOR="#00ffff"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Network Topology
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="&#x51b3;&#x5b9a;&#x7f51;&#x7edc;&#x62d3;&#x6251;&#x7684;&#x4e09;&#x4e2a;&#x53d8;&#x91cf;" ID="ID_134485367" CREATED="1487636659832" MODIFIED="1487636679111">
+<node TEXT="&#x4ea4;&#x6362;&#x673a;&#x6807;&#x8bc6;&#x7b26;" ID="ID_1360788201" CREATED="1487636688346" MODIFIED="1487636783280"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#20132;&#25442;&#26426;&#30340;&#21807;&#19968;MAC&#22320;&#22336;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="&#x5404;&#x4e2a;&#x4ea4;&#x6362;&#x673a;&#x7aef;&#x53e3;&#x5230;&#x6839;&#x6865;&#x7684;&#x8def;&#x5f84;&#x5f00;&#x9500;" ID="ID_219407617" CREATED="1487636713631" MODIFIED="1487636983530"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <span http-equiv="content-type" content="text/html; charset=utf-8" style="color: rgb(51, 51, 51); font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol; font-size: 16px; font-style: normal; font-weight: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none"><font color="rgb(51, 51, 51)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol" size="16px">802.1D&#35268;&#26684;&#20998;&#37197;16&#20301;&#65288;&#30701;&#25972;&#25968;&#65289;&#22522;&#20110;&#31471;&#21475;&#24102;&#23485;&#30340;&#40664;&#35748;&#31471;&#21475;&#24320;&#38144;&#20540;&#32473;&#27599;&#20010;&#31471;&#21475;&#12290;&#22240;&#20026;&#31649;&#29702;&#21592;&#21516;&#26102;&#26377;&#30528;&#25163;&#21160;&#20998;&#37197;&#31471;&#21475;&#24320;&#38144;&#20540;&#65288;1&#21644;65535&#20043;&#38388;&#65289;&#30340;&#33021;&#21147;&#65292;&#25152;&#20197;&#35813;16&#20301;&#20540;&#23601;&#21482;&#29992;&#22312;&#37027;&#20123;&#26410;&#26377;&#20855;&#20307;&#37197;&#32622;&#20102;&#31471;&#21475;&#24320;&#38144;&#30340;&#31471;&#21475;&#12290;</font></span>
+    </p>
+    <p>
+      
+    </p>
+    <p http-equiv="content-type" content="text/html; charset=utf-8" style="margin-top: 0px; margin-bottom: 16px; color: rgb(51, 51, 51); font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol; font-size: 16px; font-style: normal; font-weight: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px">
+      &#19979;&#34920;&#21015;&#20986;&#20102;&#22312;&#24212;&#29992;&#30701;&#25972;&#25968;&#26041;&#24335;&#35745;&#31639;&#31471;&#21475;&#24320;&#38144;&#26102;&#21508;&#31181;&#31867;&#22411;&#31471;&#21475;&#30340;&#40664;&#35748;&#20540;&#12290;
+    </p>
+    <p style="margin-top: 0px; margin-bottom: 16px; color: rgb(51, 51, 51); font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol; font-size: 16px; font-style: normal; font-weight: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px">
+      <em>&#34920; -- &#40664;&#35748;STP&#31471;&#21475;&#24320;&#38144;&#20540;</em>
+    </p>
+    <table style="border-spacing: 0px; margin-top: 0px; margin-bottom: 16px; display: block; width: 888px; color: rgb(51, 51, 51); font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol; font-size: 16px; font-style: normal; font-weight: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px">
+      <tr style="background-color: rgb(255, 255, 255)">
+        <th style="padding-top: 6px; padding-bottom: 6px; padding-right: 13px; padding-left: 13px; font-weight: 600">
+          &#24102;&#23485;
+        </th>
+        <th style="padding-top: 6px; padding-bottom: 6px; padding-right: 13px; padding-left: 13px; font-weight: 600">
+          &#40664;&#35748;&#31471;&#21475;&#24320;&#38144;
+        </th>
+      </tr>
+      <tr style="background-color: rgb(248, 248, 248)">
+        <td style="padding-top: 6px; padding-bottom: 6px; padding-right: 13px; padding-left: 13px">
+          4Mbps
+        </td>
+        <td style="padding-top: 6px; padding-bottom: 6px; padding-right: 13px; padding-left: 13px">
+          250
+        </td>
+      </tr>
+      <tr style="background-color: rgb(255, 255, 255)">
+        <td style="padding-top: 6px; padding-bottom: 6px; padding-right: 13px; padding-left: 13px">
+          10Mbps
+        </td>
+        <td style="padding-top: 6px; padding-bottom: 6px; padding-right: 13px; padding-left: 13px">
+          100
+        </td>
+      </tr>
+      <tr style="background-color: rgb(248, 248, 248)">
+        <td style="padding-top: 6px; padding-bottom: 6px; padding-right: 13px; padding-left: 13px">
+          16Mbps
+        </td>
+        <td style="padding-top: 6px; padding-bottom: 6px; padding-right: 13px; padding-left: 13px">
+          62
+        </td>
+      </tr>
+      <tr style="background-color: rgb(255, 255, 255)">
+        <td style="padding-top: 6px; padding-bottom: 6px; padding-right: 13px; padding-left: 13px">
+          100Mbps
+        </td>
+        <td style="padding-top: 6px; padding-bottom: 6px; padding-right: 13px; padding-left: 13px">
+          19
+        </td>
+      </tr>
+      <tr style="background-color: rgb(248, 248, 248)">
+        <td style="padding-top: 6px; padding-bottom: 6px; padding-right: 13px; padding-left: 13px">
+          1Gbps
+        </td>
+        <td style="padding-top: 6px; padding-bottom: 6px; padding-right: 13px; padding-left: 13px">
+          4
+        </td>
+      </tr>
+      <tr style="background-color: rgb(255, 255, 255)">
+        <td style="padding-top: 6px; padding-bottom: 6px; padding-right: 13px; padding-left: 13px">
+          10Gbps
+        </td>
+        <td style="padding-top: 6px; padding-bottom: 6px; padding-right: 13px; padding-left: 13px">
+          2
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="&#x5404;&#x4e2a;&#x4ea4;&#x6362;&#x673a;&#x7aef;&#x53e3;&#x7684;&#x7aef;&#x53e3;&#x8bc6;&#x522b;&#x7b26;" ID="ID_330467769" CREATED="1487636744584" MODIFIED="1487639025412"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#35813;&#31471;&#21475;&#30340;MAC&#22320;&#22336;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="&#x89d2;&#x8272;" POSITION="right" ID="ID_1259001063" CREATED="1487582630594" MODIFIED="1487636290029" HGAP="0" VSHIFT="-130">
+<edge COLOR="#ff0000"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#22312;&#20855;&#22791;&#20887;&#20313;&#30340;&#20132;&#25442;&#32593;&#32476;&#25299;&#25169;&#20013;&#65292;&#20801;&#35768;&#23384;&#22312;&#22810;&#26465;&#29289;&#29702;&#38142;&#36335;&#30340;&#21516;&#26102;, &#36890;&#36807;&#24314;&#31435;&#19968;&#20010;&#26080;&#24490;&#29615;&#36923;&#36753;&#25299;&#25169;&#65292;&#38459;&#27490;&#32593;&#32476;&#19978;&#24490;&#29615;&#30340;&#21457;&#29983;
+    </p>
+  </body>
+</html>
+
+</richcontent>
 </node>
 </node>
 </map>
