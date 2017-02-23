6e30f563be6df3001c6ff181dfc31bcbf7d90a2f
Peng Hailin <unisko@gmail.com>
2017-02-23 08:25:55 +0800

    Daily update. 2017-02-23 08:25:55

--- git-push.sh
+++ git-push.sh
@@ -7,6 +7,7 @@ push () {
     git add images/*.png
     git add pdfs/*.pdf
     git add mindmaps/*.mm
+    git add mindmaps/*.md
     git commit -m "$1"
     push_success=0
     push_tries=0
---
+++ mindmaps/README.md
@@ -0,0 +1,1 @@
+这些mindmaps是Freeplane的。所以要用[此软件](https://www.freeplane.org/wiki/index.php/Main_Page)来打开。
