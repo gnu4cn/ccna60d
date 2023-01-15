f9132a55d8e026ec1916e15d4b3d0185fc51aa83
Unisko PENG, <unisko@gmail.com>
2023-01-15 20:30:20 +0800

    Updated.

--- custom_script.js
+++ custom_script.js
@@ -1,5 +1,15 @@
-var honkit_link = $('a[class=gitbook-link]');
+require(['gitbook', 'jQuery'], function(gitbook, $) {
+  var honkit_link = $('a[class=gitbook-link]');
 
-var text = honkit_link.text();
-honkit_link.parent().html(`<span style="align: center;">${text}</span>`);
+  var text = honkit_link.text();
+  honkit_link.parent().html(`<span style="align: center;">${text}</span>`);
 
+  //
+  gitbook.events.bind('page.change', function() {
+
+    setTimeout(() => {
+      var el = $('div.book-body');
+      el.attr('style', '');
+    }, 300);
+  });
+});
