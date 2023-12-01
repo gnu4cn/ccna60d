15167cf36e93545b339ba45a9f607b831f12cd92
rust-lang.xfoss.com <unisko@gmail.com>
2023-12-01 14:36:33 +0800

    Updated the last-updated.

--- theme/pagetoc.js
+++ theme/pagetoc.js
@@ -65,3 +65,12 @@ if(document.querySelector("#document-not-found404")) {
     }
   }, 1000);
 }
+
+var f = document.getElementById("last-change");
+if (f) {
+  var text = f.innerHTML;
+  text = text.replace("Last change", "知识共享许可协议，CC - 最后修改")
+  text = text.replace("commit", "于提交")
+
+  f.innerHTML = text
+}
